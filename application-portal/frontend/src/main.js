29cc68e46e20072748e32bd056ed36eb93fb2a0c
Sergej Bakanow <81242217+SBakanow@users.noreply.github.com>
2025-04-23 12:55:54 +0200

    Add pinia to app

--- application-portal/frontend/src/main.js
+++ application-portal/frontend/src/main.js
@@ -4,10 +4,10 @@ import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 import { createApp, h, provide } from 'vue'
 import { DefaultApolloClient } from '@vue/apollo-composable'
-
 import apolloClient from './apolloClient.js'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 const app = createApp({
   setup() {
@@ -17,6 +17,9 @@ const app = createApp({
   render: () => h(App),
 })
 
+const pinia = createPinia()
+
+app.use(pinia)
 app.use(router)
 app.use(Toast)
 
