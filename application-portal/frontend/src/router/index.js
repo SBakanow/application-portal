008331735954685a5390b931668f6de4961e5514
Sergej Bakanow <81242217+SBakanow@users.noreply.github.com>
2025-04-23 12:57:48 +0200

    Add navigation guard

--- application-portal/frontend/src/router/index.js
+++ application-portal/frontend/src/router/index.js
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/store/authStore'
 import HomeView from '@/views/HomeView.vue'
 import ApplicationsView from '@/views/ApplicationsView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import ApplicationView from '@/views/ApplicationView.vue'
 import AddApplicationView from '@/views/AddApplicationView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,21 +15,35 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
     {
       path: '/applications',
       name: 'applications',
       component: ApplicationsView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/applications/:id',
       name: 'application',
       component: ApplicationView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/applications/add',
       name: 'add-application',
       component: AddApplicationView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/:catchAll(.*)',
@@ -34,6 +51,24 @@ const router = createRouter({
       component: NotFoundView,
     },
   ],
+})
+
+router.beforeEach(async (to, from, next) => {
+  const authStore = useAuthStore()
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    try {
+      await authStore.checkAuth()
+
+      if (authStore.isAuthenticated) {
+        return next()
+      }
+    } catch (error) {
+      console.warn('Authentication failed. Redirecting to login.')
+    }
+    return next({ name: 'login' })
+  }
+  next()
 })
 
 export default router
